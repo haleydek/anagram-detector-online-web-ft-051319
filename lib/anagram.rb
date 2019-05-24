8f914bd779a958c71eb0bdc1a1304cc13ec74bd2
Haley DeKeyser <hcd09@live.com>
2019-05-24 03:26:37 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
   
   def match(potential_anagrams)
     %w(potential_anagrams).find_all do |some_word|
-      some_word.split("").sort == @word.to_a.sort
+      some_word.split("").sort == @word.split().sort
     end
   end
   
